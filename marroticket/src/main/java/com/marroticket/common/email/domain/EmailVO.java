65a4a89c21ad5e3dcfdf97cdbf10d035c2e40fca
hans9810 <hsh5331@naver.com>
2023-02-03 10:32:23 +0900

    board 3(notice/tfaq/ufaq) complete

---
+++ marroticket/src/main/java/com/marroticket/common/email/domain/EmailVO.java
@@ -0,0 +1,17 @@
+package com.marroticket.common.email.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+public class EmailVO {
+	private String address;
+	private String title;
+	private String password;
+	private String id;
+	private final String umemberLoginUrl ="http://localhost:8080/login/umemberlogin";
+	private final String tmemberLoginUrl ="http://localhost:8080/login/tmemberlogin";
+}
